359ef7f769b5bbe4dcd122dfb9df3469b0a88b17
BoshuLi <liboshu123@gmail.com>
2016-05-12 16:28:28 +0200

    First
    Fist attempt

---
+++ src/main/java/es/upm/ontology/PROTOCOL_REGISTRATION.java
@@ -0,0 +1,14 @@
+package es.upm.ontology;
+
+import jade.content.*;
+import jade.util.leap.*;
+import jade.core.*;
+
+/**
+* Protege name: PROTOCOL_REGISTRATION
+* @author ontology bean generator
+* @version 2016/05/8, 17:03:39
+*/
+public class PROTOCOL_REGISTRATION extends Protocol{ 
+
+}
---
+++ src/main/java/es/upm/ontology/ReleaseCapsule.java
@@ -0,0 +1,26 @@
+package es.upm.ontology;
+
+
+import jade.content.*;
+import jade.util.leap.*;
+import jade.core.*;
+
+/**
+* Protege name: ReleaseCapsule
+* @author ontology bean generator
+* @version 2016/05/8, 17:03:39
+*/
+public class ReleaseCapsule implements AgentAction {
+
+   /**
+* Protege name: location
+   */
+   private Location location;
+   public void setLocation(Location value) { 
+    this.location=value;
+   }
+   public Location getLocation() {
+     return this.location;
+   }
+
+}
